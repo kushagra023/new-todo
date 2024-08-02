126f344118cbacc8b69ff4ecd4d26508aabc0f2c
Kushagra Agarwal <138867347+kushagra023@users.noreply.github.com>
2024-08-02 17:33:23 +0530

    Update urls.py

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('todos/', include('todos.urls')),
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('metrics/', exports.ExportView.as_view(), name='prometheus-metrics'),  # Add metrics endpoint
+    path('metrics/', MetricsView.as_view(), name='prometheus-metrics'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
