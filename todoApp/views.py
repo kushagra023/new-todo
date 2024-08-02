b7aaabc4be8a7174d70b3b54711a6b0d2bd488c7
Kushagra Agarwal <138867347+kushagra023@users.noreply.github.com>
2024-08-02 17:47:50 +0530

    Update views.py

--- todoApp/views.py
+++ todoApp/views.py
@@ -1,11 +1,10 @@
 from django.http import HttpResponse
-from django_prometheus.exports import ExportToPrometheus
+from django_prometheus.exports import PrometheusMetrics
 from django.views import View
 
 class MetricsView(View):
     content_type = 'text/plain; charset=utf-8'
 
     def get(self, request, *args, **kwargs):
-        # Generate Prometheus metrics content
-        content = ExportToPrometheus().get_metrics()
-        return HttpResponse(content, content_type=self.content_type)
+        metrics = PrometheusMetrics().generate_latest()
+        return HttpResponse(metrics, content_type=self.content_type)
