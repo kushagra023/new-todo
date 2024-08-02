38bd44dc38acea576e5ff59759e6fad4b0c9d628
Kushagra Agarwal <138867347+kushagra023@users.noreply.github.com>
2024-08-02 23:10:13 +0530

    Update views.py

--- todoApp/views.py
+++ todoApp/views.py
@@ -1,10 +1,4 @@
-from django.http import HttpResponse
-from django_prometheus.exports import PrometheusMetrics
-from django.views import View
+from django.shortcuts import redirect
 
-class MetricsView(View):
-    content_type = 'text/plain; charset=utf-8'
-
-    def get(self, request, *args, **kwargs):
-        metrics = PrometheusMetrics().generate_latest()
-        return HttpResponse(metrics, content_type=self.content_type)
+def index(request):
+    return redirect('/todos')
