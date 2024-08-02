d07466066a5e90094b600a9b84b98fb72c6bc5ae
Kushagra Agarwal <138867347+kushagra023@users.noreply.github.com>
2024-08-02 17:39:44 +0530

    Update urls.py

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import settings, views
 from django_prometheus import exports  # Import Prometheus export
+from .views import MetricsView  # Import MetricsView
 
 urlpatterns = [
     path('todos/', include('todos.urls')),
